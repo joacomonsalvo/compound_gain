da7c8021fc8bf55cfa1c11e6459815bafdc664aa
joacomonsalvo <jmonsalvo.business@protonmail.com>
2022-11-10 23:16:08 -0300

    v2

--- compound_gain.py
+++ compound_gain.py
@@ -3,3 +3,10 @@ def compound_gain(money:int, days:int, int_rate:float):
     for i in range(0, days):
         counter = counter * int_rate
     return counter
+
+
+if __name__ == "__main__":
+    money = int(input("Insert the initial amount of money to be invested: \n")) 
+    days = int(input("Insert how much days the money will be invested: \n"))
+    int_rate = 1.00 + (float(input("Insert interest rate per day: \n")) / 100)
+    print(compound_gain(money, days, int_rate))
